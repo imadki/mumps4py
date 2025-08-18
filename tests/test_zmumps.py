d0daabb65ba50be825cf51e6e20fe1bd1d8b08b2
williampiat3 <32038421+williampiat3@users.noreply.github.com>
2025-08-18 16:13:08 +0200

    testing system

--- tests/test_zmumps.py
+++ tests/test_zmumps.py
@@ -11,10 +11,12 @@ def test_solve_single():
     dtype = np.complex128
 
     n = 5
-    irn = np.array([0,1,3,4,1,0,4,2,1,2,0,2], dtype=np.int32)
-    jcn = np.array([1,2,2,4,0,0,1,3,4,1,2,2], dtype=np.int32)
-    a = np.array([3.0,-3.0,2.0,1.0,3.0,2.0,4.0,2.0,6.0,-1.0,4.0,1.0], dtype=dtype)
-    b = np.array([20.0,24.0,9.0,6.0,13.0], dtype=dtype)
+    irn = np.array([0,0,0,1,1,1,2,2,2,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,9], dtype=np.int32)
+    jcn = np.array([0,2,4,0,3,5,1,3,4,0,7,8,1,6,9,2,6,7,3,8,9,4,7,9,5,6,8,6], dtype=np.int32)
+    a = np.array([-1.+0.j, 1.+0.j,-1.+0.j, 1.+0.j, -1.+0.j,  1.+0.j,  1.+0.j,  1.+0.j, 1.+0.j,  0.+10.j,
+                  -1.+0.j,  1.+0.j,  0.+10.j, -1.+0.j,  1.+0.j,  1.+0.j, -1.+0.j,  1.+0.j,  1.+0.j, -1.+0.j,
+                  1.+0.j,  1.+0.j,  0.-10.j,  0.+10.j, 1.+0.j,  0.-10.j,  0.+10.j,  1.+0.j], dtype=dtype)
+    b = np.array([[-10.,0.,0.,0.,0.,0.,0.,0.,0.,0.]], dtype=dtype)
 
     solver.set_rcd_centralized(irn+1, jcn+1, a, n)
     solver._mumps_call(job=1)
@@ -25,5 +27,4 @@ def test_solve_single():
     solver._mumps_call(3)
 
     if MPI.COMM_WORLD.Get_rank()==0:
-        assert np.allclose(rhs, [np.complex128(1), np.complex128(2), np.complex128(3),
-                                 np.complex128(4), np.complex128(5)], atol=1e-10)
+        assert np.allclose(rhs, [0.04950495-4.95049505e-01j, -0.04950495+4.95049505e-01j, -5.-2.22044605e-16j, -4.9009901 -9.90099010e-01j, 4.95049505+4.95049505e-01j, -4.95049505-4.95049505e-01j, 0.+0.00000000e+00j, 5.+2.22044605e-16j, 0.04950495-4.95049505e-01j, 4.95049505+4.95049505e-01j], atol=1e-6)
